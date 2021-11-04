8da1ca1009179e04b79db4a7603ee5c8d490b2b2
Blake Gilmore <blakeg435@gmail.com>
2021-11-03 20:25:16 -0700

    Hello World

---
+++ Anki/Nov3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned short Val1 = 2;
+    unsigned int Val2 = 3;
+    unsigned long Val3 = 56000;
+
+    cout << Val1;
+    cout << Val2;
+    cout << Val3;
+
+    cout << "\n";
+
+    int feet = 0;
+    int inches = 0;
+
+    int feet2 (0);
+    int inches2 (0);
+
+    cout << feet;
+    cout << feet2;
+    cout << inches;
+    cout << inches2;
+    
+
+    return 0;
+}
