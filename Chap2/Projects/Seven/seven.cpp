d61630d4c43c09b8dfcd2bf8289bb578a8e1b459
Blake Gilmore <blakeg435@gmail.com>
2021-11-09 16:53:01 -0800

    Score Exercise

---
+++ Chap2/Projects/Seven/seven.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int exerciseNumber = 0;
+    double totalPercentage = 0;
+    double currentScore = 0;
+    double pointsPossible = 0;
+    double totalPoints = 0;
+    double pointsReceived = 0;
+
+    cout << "How many exercises: ";
+    cin >> exerciseNumber;
+
+    for (int i = 1; i <= exerciseNumber; i++)
+    {
+        cout << "Score received for exercise " << i << ":";
+        cin >> currentScore;
+
+        cout << "Total points possible for exercise " << i << ":";
+        cin >> pointsPossible;
+
+        totalPoints += pointsPossible;
+        pointsReceived += currentScore;
+
+        totalPercentage += (currentScore / pointsPossible);
+    }
+
+    totalPercentage = totalPercentage / exerciseNumber;
+    totalPercentage *= 100;
+
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+
+
+    cout << "Your total is " << pointsReceived << " out of " << totalPoints << ", or %" << totalPercentage << endl;
+
+    return 0;
+}
