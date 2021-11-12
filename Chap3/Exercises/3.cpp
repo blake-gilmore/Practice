c2e85957834863501c8d68812cd7c97509165f8f
Blake Gilmore <blakeg435@gmail.com>
2021-11-12 14:13:16 -0800

    exercise

---
+++ Chap3/Exercises/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double root = 0.0;
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "Square root of " << i << ": " << sqrt(i) << endl;
+    }
+    return 0;
+}
