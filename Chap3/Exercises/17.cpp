62f38c8fdaceddc774b25d2945301f36fb0f5b6f
Blake Gilmore <blakeg435@gmail.com>
2021-11-12 16:01:50 -0800

    test

--- Chap3/Exercises/17.cpp
+++ Chap3/Exercises/17.cpp
@@ -11,6 +11,16 @@ int main()
     cin >> num1 >> num2 >> num3;
 
     sum(num1, num2, num3);
+
+    for (int i = 0; i < 3; i++)
+{
+    int x = 0;
+    cout << x;
+    for (int j = 0; j < 3; j++)
+    {
+        cout << i;
+    }
+}
     return 0;
 }
 
@@ -18,6 +28,8 @@ void sum(int num1, int num2, int num3)
 {
     cout << (num1 + num2 + num3);
 }
+
+
 
 //number is number wanting to get square root of
 //computes and returns the square root of the number
