9b24166e2b24f5028f22ecfa750911c1ab33abe5
Blake Gilmore <blakeg435@gmail.com>
2021-12-15 15:51:50 -0800

    money

---
+++ Chap6/Eight/eight.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+class Money
+{
+public:
+    void setDollars(int);
+    void setCents(int);
+    int getDollars();
+    int getCents();
+    double decimalAmount();
+    void input();
+private:
+    int dollars;
+    int cents;
+};
+int main()
+{
+    Money money1, money2;
+    double temp;
+    money1.input();
+    money2.input();
+    cout << "Output money 1 or 2 amount: ";
+    cin >> temp;
+    if (temp == 1)
+    {
+        cout << "$" << money1.getDollars << "." << money1.getCents << endl;
+    }
+    else if (temp == 2)
+    {
+        cout << "$" << money2.getDollars << "." << money2.getCents << endl;
+    }
+
+    
+}
+void Money::input()
+{
+    cout << "Enter dollars: ";
+    cin >> dollars;
+    cout << "Enter cents: ";
+    cin >> cents;
+    return;
+}
