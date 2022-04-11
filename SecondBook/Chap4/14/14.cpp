bcdcaa4098b14dfa0dea6e5b6bbcba2801f5d57c
Blake Gilmore <blakeg435@gmail.com>
2022-04-11 16:39:53 -0700

    paper

---
+++ SecondBook/Chap4/14/14.cpp
@@ -0,0 +1,12 @@
+//Algorithm
+/* 1 inch = 72 points
+1. Get Length, Width of paper
+2. Get top, bottom, left, and right margins
+3. Get point size of a line
+4. Get double or single spaced
+5. If double spaced, double point size of each character
+6. Subtract margins from length and width
+7. 
+
+
+*/
