15af226df263f4972c7e43a65b92b3c1dd0710aa
Blake Gilmore <blakeg435@gmail.com>
2022-05-09 11:04:27 -0700

    more

--- SecondBook/Chap10/examples/10-6.cpp
+++ SecondBook/Chap10/examples/10-6.cpp
@@ -8,6 +8,8 @@ public:
     inventory(string);
     inventory(string, int, double);
     inventory(string, int, double, int);
+    //can all just be
+    inventory(string = "", int = 0, double  = 0.0, int = 0);
 
 private:
     string name;
@@ -23,3 +25,18 @@ inventory::inventory()
     price = 0.0;
     unitsInStock = 0;
 }
+
+inventory item1;
+inventory item2("Dryer");
+inventory item3("Washer", 2345, 278.95);
+
+//default parameters
+clockType clockType(int = 0, int = 0, int = 0);
+clockType clock1;
+clockType clock2(5);
+clockType clock3(12,30)...
+
+//Destructors
+~clockType();
+
+
