aa8ef559f22a8902cb651e04040b1880b0280745
Blake Gilmore <blakeg435@gmail.com>
2022-06-01 13:31:08 -0700

    new

---
+++ SecondBook/Chap13/Exercises/2/main.cpp
@@ -0,0 +1,36 @@
+/*C8648_CH13
+P R O G R A M M I N G E X E R C I S E S
+1. This chapter uses the class rectangleType to illustate how to overload the
+operators +, *, ==, !=, >>, and <<. In this exercise, first redefine the class
+rectangleType by declaring the instance variables as protected and then
+overload additional operators as defined in parts a to c.
+a. Overload the pre- and post-increment and decrement operators to
+increment and decrement, respectively, the length and width of a
+rectangle by one unit. (Note that after decrementing the length and
+width, they must be postive.)
+b. Overload the binary operator - to subtract the dimensions of one
+rectangle from the corresponding dimensions of another rectangle. If
+the resulting dimensions are not positive, output an appropriate message
+and do not perform the operation.
+c. The operators == and != are overloaded by considering the lengths and
+widths of rectangles. Redefine the functions to overload the relational
+operator by considering the areas of rectangles as follows: Two rectan-
+gles are the same, if they have the same area; otherwise, the rectangles
+are not the same. Similary, rectangle yard1 is greater than rectangle
+yard2 if the area of yard1 is greater than the area of yard2 . Overload
+the remaining relational operators using similar definitions.
+d. Write the definitions of the functions to overload the operators defined
+in parts a to c.
+e. Write a test program that tests various operations on the
+class rectangleType.
+
+Redo Programming Exercise 1 by overloading the operators as
+nonmembers of the class rectangleType.
+b. Write a test program that tests various operations on the
+class rectangleType.*/
+
+
+int main()
+{
+    return 0;
+}
