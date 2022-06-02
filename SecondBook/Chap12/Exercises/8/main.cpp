bf408b13879b81786620680821a557b0d572527b
Blake Gilmore <blakeg435@gmail.com>
2022-06-02 11:55:44 -0700

    Mystring

---
+++ SecondBook/Chap12/Exercises/8/main.cpp
@@ -0,0 +1,20 @@
+/*Extend the definition of the class newString as follows:
+i. Overload the operators + and += to perform the string concatena-
+tion operations.
+ii. Add the function length to return the length of the string.
+b. Write the definition of the function to implement the operations defined
+in part a.
+c. Write a test program to test various operations on the newString objects*/
+
+
+#include "newString.h"
+
+int main()
+{
+    newString stringOne;
+    newString stringTwo;
+
+
+
+    return 0;
+}
---
+++ SecondBook/Chap12/Exercises/8/newString.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <cassert>
+#include "newString.h"
+using namespace std;
+//Constructor: conversion from the char string to newString
+newString::newString(const char *str)
+{
+strLength = strlen(str);
+strPtr = new char[strLength + 1]; //allocate memory to
+//store the char string
+strcpy(strPtr, str); //copy string into strPtr
+}
+newString::newString()
+{
+strLength = 0;
+strPtr = new char[1];
+strcpy(strPtr, "");
+}
+newString::newString(const newString& rightStr) //copy constructor
+{
+strLength = rightStr.strLength;
+strPtr = new char[strLength + 1];
+strcpy(strPtr, rightStr.strPtr);
+}
+newString::~newString() //destructor
+{
+delete [] strPtr;
+}
+//overload the assignment operator
+const newString& newString::operator=(const newString& rightStr)
+{
+if (this != &rightStr) //avoid self-copy
+{
+delete [] strPtr;
+strLength = rightStr.strLength;
+strPtr = new char[strLength + 1];
+strcpy(strPtr, rightStr.strPtr);
+}
+return *this;
+}
+char& newString::operator[] (int index)
+{
+assert(0 <= index && index < strLength);
+return strPtr[index];
+}
+const char& newString::operator[](int index) const
+{
+assert(0 <= index && index < strLength);
+return strPtr[index];
+}
+//Overload the relational operators.
+bool newString::operator==(const newString& rightStr) const
+{
+return (strcmp(strPtr, rightStr.strPtr) == 0);
+}
+bool newString::operator<(const newString& rightStr) const
+{
+return (strcmp(strPtr, rightStr.strPtr) < 0);
+}
+bool newString::operator<=(const newString& rightStr) const
+{
+return (strcmp(strPtr, rightStr.strPtr) <= 0);
+}
+bool newString::operator>(const newString& rightStr) const
+{
+return (strcmp(strPtr, rightStr.strPtr) > 0);
+}
+bool newString::operator>=(const newString& rightStr) const
+{
+return (strcmp(strPtr, rightStr.strPtr) >= 0);
+}
+bool newString::operator!=(const newString& rightStr) const
+{
+return (strcmp(strPtr, rightStr.strPtr) != 0);
+}
+//Overload the stream insertion operator <<
+ostream& operator << (ostream& osObject, const newString& str)
+{
+osObject << str.strPtr;
+return osObject;
+}
+//Overload the stream extraction operator >>
+istream& operator >> (istream& isObject, newString& str)
+{
+char temp[81];
+isObject >> setw(81) >> temp;
+str = temp;
+return isObject;
+}
+
+newString newString::operator+(const newString& inString) const
+{
+    newString tempString;
+    tempString.strLength = strLength + inString.strLength;
+    tempString.strPtr = new char[strLength + 1];
+    strcpy(tempString.strPtr, strPtr);
+    strcat(tempString.strPtr, inString.strPtr);
+    return tempString;
+}
+newString newString::operator+=(const newString& inString) const
+{
+
+}
