4e0a72d4afb905f2dd885c9bbb8e11751e2d8db4
Blake Gilmore <blakeg435@gmail.com>
2022-06-06 12:16:00 -0700

    array overloading

--- SecondBook/Chap12/Exercises/11/main.cpp
+++ SecondBook/Chap12/Exercises/11/main.cpp
@@ -6,5 +6,16 @@ int main()
     myArray<int> intArray(-5, 4);
     intArray.initializeArray();
     
+    std::cout << "Enter the number of the array you want to get: ";
+    int tempInt;
+    std::cin >> tempInt;
+    std::cout << "The element at that number is " << intArray[tempInt];
+    exit;
+
+    std::cin >> tempInt;
+    int tempElement;
+    std::cout << "Enter the element: ";
+    std::cin >> tempElement;
+    //intArray.setElement(tempElement);
     return 0;
 }
--- SecondBook/Chap12/Exercises/11/myArray.h
+++ SecondBook/Chap12/Exercises/11/myArray.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 template<class ArrayType>
 class myArray
@@ -9,6 +10,7 @@ public:
     ArrayType getElement(int);
     void setElement(int);
     ~myArray();
+    ArrayType& operator[](int);
 
 protected:
     ArrayType* typePtr;
@@ -46,4 +48,30 @@ myArray<ArrayType>::~myArray()
     delete[] typePtr;
     typePtr = NULL;
     return;
+}
+
+template <class ArrayType>
+ArrayType myArray<ArrayType>::getElement(int index)
+{
+    int difference = abs(0 - arrayStart);
+    if (index > arrayEnd || index < arrayStart)
+    {
+        std::cout << "Out of bound. exiting.\n";
+        exit;
+    }
+        
+    return (typePtr[index + difference]);
+}
+
+template <class ArrayType>
+ArrayType& myArray<ArrayType>::operator[](int index)
+{
+    int difference = abs(0 - arrayStart);
+    if (index > arrayEnd || index < arrayStart)
+    {
+        std::cout << "Out of bound. exiting.\n";
+        exit;
+    }
+        
+    return (typePtr[index + difference]);
 }
