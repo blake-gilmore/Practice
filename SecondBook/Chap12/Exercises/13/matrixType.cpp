0f43e11033be94ddeadd30674682d77b8618fcc1
Blake Gilmore <blakeg435@gmail.com>
2022-06-08 12:49:56 -0700

    Matrix addition

---
+++ SecondBook/Chap12/Exercises/13/matrixType.cpp
@@ -0,0 +1,104 @@
+#include "matrixType.h"
+#include <iomanip>
+
+
+matrixType::matrixType(int rowsIn, int columnsIn)
+{
+    initializeMatrix(rowsIn, columnsIn);
+    return;
+}
+void matrixType::fillMatrix()
+{
+    int tempNum;
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            std::cout << "Enter row " << i << " column " << j << ":";
+            std::cin >> tempNum;
+            matrixValues[i][j] = tempNum; 
+        }
+    }
+}
+
+void matrixType::initializeMatrix(int rowsIn, int columnsIn)
+{
+    try
+    {   
+        if (rowsIn < 0 || columnsIn < 0)
+            throw 0;
+        rows = rowsIn;
+        columns = columnsIn;
+        if (rows == 0 || columns == 0)
+            matrixValues = nullptr;
+        else
+        {
+            matrixValues = new int*[rows];
+            for (int i = 0; i < rows; i++)
+                matrixValues[i] = new int[columns];
+        }
+    }
+    catch (int)
+    {
+        std::cout << "Invalid column or row. Matrix not initialized.";
+        matrixValues = nullptr;
+    }
+
+    return;
+}
+matrixType matrixType::operator +(const matrixType& matrixIn)
+{
+    matrixType tempMatrix;
+    if (rows == matrixIn.rows && columns == matrixIn.columns)
+    {
+        tempMatrix.initializeMatrix(rows, columns);
+        for (int i = 0; i < rows; i ++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                tempMatrix.matrixValues[i][j] = matrixValues[i][j] + matrixIn.matrixValues[i][j];
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Rows and columns are not the same. Addition not done.";
+    }
+
+    return tempMatrix;
+}
+matrixType matrixType::operator -(matrixType&)
+{
+    matrixType tempMatrix;
+    return tempMatrix;
+}
+matrixType matrixType::operator *(matrixType&)
+{
+    matrixType tempMatrix;
+    return tempMatrix;
+}
+matrixType& matrixType::operator =(matrixType&)
+{
+    return *this;
+}
+std::ostream& operator <<(std::ostream& osIn, const matrixType& matrixIn)
+{
+    std::cout << std::endl;
+    for (int i = 0; i < matrixIn.rows; i++)
+    {
+        for (int j = 0; j < matrixIn.columns; j++)
+        {
+            std::cout << std::setw(5) << std::left << matrixIn.matrixValues[i][j];
+        }
+        std::cout << "\n\n";
+    }
+    return osIn;
+}
+
+matrixType::~matrixType()
+{
+    for (int i = 0; i < rows; i++)
+        delete[] matrixValues[i];
+    delete[] matrixValues;
+    matrixValues = nullptr; 
+}
---
+++ SecondBook/Chap12/Exercises/13/matrixType.h
@@ -0,0 +1,19 @@
+#include <iostream>
+class matrixType
+{
+public:
+    matrixType(int = 0, int = 0);
+    void initializeMatrix(int, int);
+    matrixType operator +(const matrixType&);
+    matrixType operator -(matrixType&);
+    matrixType operator *(matrixType&);
+    matrixType& operator =(matrixType&);
+    friend std::ostream& operator <<(std::ostream&, const matrixType&);
+    void fillMatrix();
+    ~matrixType();
+private:
+    int rows;
+    int columns;
+    int** matrixValues;
+};
+
