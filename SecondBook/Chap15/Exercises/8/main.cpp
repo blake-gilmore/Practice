14e6ffda0295cc8fcdcae232fa5e84d1f2d30464
Blake Gilmore <blakeg435@gmail.com>
2022-06-13 16:33:44 -0700

    increasing integer

---
+++ SecondBook/Chap15/Exercises/8/main.cpp
@@ -0,0 +1,3 @@
+/*Write a recursive function that returns true if the digits of a positive integer
+are in increasing order; otherwise, the function returns false. Also, write a
+program to test your function*/
