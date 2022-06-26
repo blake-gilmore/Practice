2a1b9ccc412c1d169201b26aa41d0de5476a5aa3
Blake Gilmore <blakeg435@gmail.com>
2022-06-25 21:36:36 -0700

    .

--- SecondBook/Chap16/Exercises/19/main.cpp
+++ SecondBook/Chap16/Exercises/19/main.cpp
@@ -6,14 +6,14 @@ int recursiveBinarySearch(int* arrayIn, int value, int numinArray, int startingI
 {
     int middleIndex = (numinArray / 2) + startingIndex;
     
-    if (middleIndex == 0)
+    if (numinArray == 0)
         return -1;
     else if (arrayIn[middleIndex] == value)
         return middleIndex;
     else if (arrayIn[middleIndex] < value)
         return recursiveBinarySearch(arrayIn, value, (numinArray - (middleIndex + 1)), middleIndex + 1);
     else
-        return recursiveBinarySearch(arrayIn, value, ((middleIndex - 1) - startingIndex), startingIndex);
+        return recursiveBinarySearch(arrayIn, value, (middleIndex - startingIndex), startingIndex);
 }
 
 int main()
