2f301c6885dca6db5085c3f298b21ada1785afeb
Blake Gilmore <blakeg435@gmail.com>
2022-06-25 21:31:36 -0700

    .

--- SecondBook/Chap16/Exercises/19/main.cpp
+++ SecondBook/Chap16/Exercises/19/main.cpp
@@ -1,4 +1,8 @@
-int recursiveBinarySearch(int* arrayIn, int value, int numinArray, int startingIndex)
+#include <iostream>
+#include <cmath>
+
+
+int recursiveBinarySearch(int* arrayIn, int value, int numinArray, int startingIndex = 0)
 {
     int middleIndex = (numinArray / 2) + startingIndex;
     
@@ -10,4 +14,21 @@ int recursiveBinarySearch(int* arrayIn, int value, int numinArray, int startingI
         return recursiveBinarySearch(arrayIn, value, (numinArray - (middleIndex + 1)), middleIndex + 1);
     else
         return recursiveBinarySearch(arrayIn, value, ((middleIndex - 1) - startingIndex), startingIndex);
+}
+
+int main()
+{
+    int sortArray[10] = {0, 1, 9, 10, 12, 13, 17, 20, 45, 100};
+
+    std::cout << "Enter the number you want: ";
+    int userNum;
+    std::cin >> userNum;
+
+    int searchIndex = recursiveBinarySearch(sortArray, userNum, 10);
+    if (searchIndex == -1)
+        std::cout << "Not found\n";
+
+    else
+        std::cout << searchIndex << std::endl;
+    return 0;
 }
